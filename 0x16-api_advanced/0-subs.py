f1ae34bf990ce3a80bb382e767ac724292cb57d3
Kidus <kingkidy50@gmail.com>
2024-05-07 19:46:32 -0700

    new user agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "MyCoolBot/1.0"}
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     respo = requests.get(url, headers=headers, allow_redirects=False)
     if respo.status_code == 200:
         data = respo.json()
