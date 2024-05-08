3cb48f4af0144b68f8b83b224ec1c427d98bf0b9
Kidus <kingkidy50@gmail.com>
2024-05-07 19:42:22 -0700

    new mando

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Function to To the Query."""
 import requests
+from requests.exceptions import HTTPError, JSONDecodeError, RequestException
 
 
 def number_of_subscribers(subreddit):
     """Total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "MyCoolBot/1.0"}
     respo = requests.get(url, headers=headers, allow_redirects=False)
     if respo.status_code == 200:
         data = respo.json()
