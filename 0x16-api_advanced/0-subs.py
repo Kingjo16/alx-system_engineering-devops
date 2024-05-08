9a639f32684a8364ee90a5b2d42a4818e08a22ea
Kidus <kingkidy50@gmail.com>
2024-05-07 18:59:56 -0700

    newq mando

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,11 @@ import requests
 def number_of_subscribers(subreddit):
     """Total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
+    headers = {"User-Agent": "Mozilla/5.0"}
     respo = requests.get(url, headers=headers, allow_redirects=False)
-    if respo.status_code == 404:
+    if respo.status_code == 200:
+        data = respo.json()
+        subs = data['data']['subscribers']
+        return subsc
+    else:
         return 0
-    resu= respo.json().get("data")
-    return resu.get("subscribers")
